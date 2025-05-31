91a20406a74011a8b25a7b2770cd71a020c339fa
Aziz0101010 <azizerabie121@gmail.com>
2025-05-31 18:59:37 +0100

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+# Create your views here.
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')
+        return render(request, 'index.html')
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        return render(request, 'profileList.html', {'profiles': profiles})
+    
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        # form for creating profile
+        return render(request, 'profileCreate.html')
